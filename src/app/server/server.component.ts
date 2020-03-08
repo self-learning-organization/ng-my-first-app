c437b2e93293bb45cee60c9a0842ace1531e02d3
Chantal Neo <Chantal@hotmail.sg>
2020-03-09 01:12:04 +0800

    String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    serverId: number = 10;              // Not required to explicitly assign the type with TypeScript's feature, because it will infer automatically
+    serverStatus: string = "offline";   // Not required to explicitly assign the type with TypeScript's feature, because it will infer automatically
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
