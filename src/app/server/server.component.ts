9508d4c303e69ba781580f55fafce1bcc371f15e
Chantal Neo <Chantal@hotmail.sg>
2020-03-09 13:15:23 +0800

    Enhancing ngIf with an Else Condition and Enhancing Server Component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent {
     serverId: number = 10;              // Not required to explicitly assign the type with TypeScript's feature, because it will infer automatically
     serverStatus: string = "offline";   // Not required to explicitly assign the type with TypeScript's feature, because it will infer automatically
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === "online" ? 'green' : 'red';
     }
 }
